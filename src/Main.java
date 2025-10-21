e76bf46b161cd3215288d10499feea49b33c9f2a
rileynicholson <rnich021@odu.edu>
2025-10-21 01:41:02 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,41 +9,4 @@ public class Main {
 		Pierre.introduction(scanner);
 		Pierre.menu(scanner);
 	}
-	
-	/*
-	
-	 IDEA: Make something that is similar to an AI that you can play games with.
-
-	-Make it so the whatever input the user enters, all the program looks for is if it contains a certain term.
-
-
-	EXCEPTION IDEA:
-	
-	System.out.println("Sorry! I didn't understand that. Could you try again?");
-
-
-
-	| * O * |
-	Woah! Nice play!
-
-	| * - * |
-
-	| ^ - ^ |
-	 
-	 */
-	
 }
-
-
-
-/* Other face ideas:
-
-| o - o |
-
-| 0 _ 0 |
-
-| ; _ ; |
-
-| T _ T |
-
-*/
