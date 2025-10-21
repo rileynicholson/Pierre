64f5a067b2a9c8a4e29d969d8be6559ebe8a439e
rileynicholson <rnich021@odu.edu>
2025-10-21 01:40:30 -0400

    Add files via upload

---
+++ src/RandomNumberGenerator.java
@@ -0,0 +1,172 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RandomNumberGenerator {
+	static Random random = new Random();
+	static Pattern pattern = Pattern.compile("\\d+");
+	
+	public static void easyGame(Scanner scanner) {
+		boolean running = true;
+		
+		String userInput;
+		int userInputDigit;
+		int randomDigit = random.nextInt(1, 11);
+		
+		System.out.println("\n| * - * | ");
+		
+		while (running) {
+			
+			System.out.println("\nI am thinking of a number between 1 and 10, what is it? (Enter in digits)");
+			userInput = scanner.nextLine();
+			
+			Matcher matcher = pattern.matcher(userInput);
+			
+			if (matcher.find()) {
+				userInput = matcher.group();
+				userInputDigit = Integer.parseInt(userInput);
+				
+				if (userInputDigit >= randomDigit) {
+					
+					Pierre.newPage();
+					System.out.println("\n| - _ - | \n" + userInputDigit + "? Go lower!");
+				}
+				
+				if (userInputDigit <= randomDigit) {
+					
+					Pierre.newPage();
+					System.out.println("\n| - _ - | \n" + userInputDigit + "? Go higher!");
+				}
+				
+				if (userInputDigit == randomDigit) {
+					
+					Pierre.newPage();
+					System.out.println("\n| ^-^ | \nYay! You got it! Congratulations! It was " + randomDigit + "!");
+					
+					try {
+						Thread.sleep(4000);
+					} catch (Exception e) {
+						
+					}
+					
+					running = false;
+					Pierre.newPage();
+					Pierre.menu(scanner);
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	public static void mediumGame(Scanner scanner) {
+		boolean running = true;
+		
+		String userInput;
+		int userInputDigit;
+		int randomDigit = random.nextInt(1, 51);
+		
+		System.out.println("\n| * - * | ");
+		
+		while (running) {
+			
+			System.out.println("\nI am thinking of a number between 1 and 50, what is it? (Enter in digits)");
+			userInput = scanner.nextLine();
+			
+			Matcher matcher = pattern.matcher(userInput);
+			
+			if (matcher.find()) {
+				userInput = matcher.group();
+				userInputDigit = Integer.parseInt(userInput);
+				
+				if (userInputDigit >= randomDigit) {
+					
+					Pierre.newPage();
+					System.out.println("\n| - _ - | \n" + userInputDigit + "? Go lower!");
+				}
+				
+				if (userInputDigit <= randomDigit) {
+					
+					Pierre.newPage();
+					System.out.println("\n| - _ - | \n" + userInputDigit + "? Go higher!");
+				}
+				
+				if (userInputDigit == randomDigit) {
+					
+					Pierre.newPage();
+					System.out.println("\n| ^-^ | \nYay! You got it! Congratulations! It was " + randomDigit + "!");
+					
+					try {
+						Thread.sleep(4000);
+					} catch (Exception e) {
+						
+					}
+					
+					running = false;
+					Pierre.newPage();
+					Pierre.menu(scanner);
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	public static void hardGame(Scanner scanner) {
+		boolean running = true;
+		
+		String userInput;
+		int userInputDigit;
+		int randomDigit = random.nextInt(1, 101);
+		
+		System.out.println("\n| * - * | ");
+		
+		while (running) {
+			
+			System.out.println("\nI am thinking of a number between 1 and 100, what is it? (Enter in digits)");
+			userInput = scanner.nextLine();
+			
+			Matcher matcher = pattern.matcher(userInput);
+			
+			if (matcher.find()) {
+				userInput = matcher.group();
+				userInputDigit = Integer.parseInt(userInput);
+				
+				if (userInputDigit >= randomDigit) {
+					
+					Pierre.newPage();
+					System.out.println("\n| - _ - | \n" + userInputDigit + "? Go lower!");
+				}
+				
+				if (userInputDigit <= randomDigit) {
+					
+					Pierre.newPage();
+					System.out.println("\n| - _ - | \n" + userInputDigit + "? Go higher!");
+				}
+				
+				if (userInputDigit == randomDigit) {
+					
+					Pierre.newPage();
+					System.out.println("\n| ^-^ | \nYay! You got it! Congratulations! It was " + randomDigit + "!");
+					
+					try {
+						Thread.sleep(4000);
+					} catch (Exception e) {
+						
+					}
+					
+					running = false;
+					Pierre.newPage();
+					Pierre.menu(scanner);
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+}
